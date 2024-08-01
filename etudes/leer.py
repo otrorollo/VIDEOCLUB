29dcef127a07cac9f7e3ec3775035c1dd3621b17
Ruben Ponce <rubenponcebusiness@gmail.com>
2024-08-01 22:25:43 +0200

    Initial commit

---
+++ etudes/leer.py
@@ -0,0 +1,3 @@
+#EJEMPLOS - MIRAR APUNTES PARA SABER COMO SE HACEN 
+fichero = open("data/generos.csv","r" ) #modo lectura-
+print(fichero.read())
---
+++ etudes/leercsv.py
@@ -0,0 +1,20 @@
+import csv
+
+fichero = open("data/directores.csv", "r", newline="")
+
+lector_csv = csv.reader(fichero, delimiter= ";", quotechar= "'")
+
+for registro in lector_csv:
+  print(registro)
+
+fichero.close()
+
+#ahora como diccionario - coge los datos de la primera fila del directorio elegido.
+fichero = open("data/directores.csv", "r", newline="")
+
+lector_csv = csv.DictReader(fichero, delimiter= ";", quotechar= "'")
+
+for registro in lector_csv:
+  print(registro)
+
+fichero.close()
