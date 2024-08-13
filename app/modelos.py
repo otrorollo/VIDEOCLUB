3f7c37188319e49291c742c8fa63ef8554eb79fe
Ruben Ponce <rubenponcebusiness@gmail.com>
2024-08-13 15:26:30 +0200

    feat: Clase pelicula compatible con futuros daos, setter y getters a la pitonica

--- app/modelos.py
+++ app/modelos.py
@@ -6,11 +6,38 @@ class Director:
     def __init__(self, nombre: str, id: int = -1):
         self.nombre = nombre
         self.id = id
+
     def __repr__(self) -> str: #Devuelve una cadena que representa al objeto de manera oficial. Está destinado a ser un string que permita recrear el objeto.
         return f"Director ({self.id}): {self.nombre}"
-    def __eq__(self, value: object) -> bool: #Define el comportamiento del operador de igualdad (==). Compara dos objetos para determinar si son iguales.
-        pass
+    
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Director):
+          return self.id == other.id and self.nombre == other.nombre
+        return False
+    
+    def __hash__(self):
+      return hash((self.id, self.nombre))
+class Pelicula:
+    def __init__(self, titulo: str, sinopsis: str, director: object, id = -1):
+        self.titulo = titulo
+        self.sinopsis = sinopsis
+        self.id = id
+        self.director = director 
 
+    @property
+    def director(self):
+        return self._director
+
+    @director.setter
+    def director(self, value):
+        if isinstance(value, Director):
+            self._director = value
+            self._id_director = value.id
+        elif isinstance(value, int):
+            self._director = None
+            self._id_director = value
+        else:
+            raise TypeError(f"{value} debe ser un entero o instancia de Director")
 
 class DAO(ABC):
     """
@@ -39,16 +66,17 @@ class DAO(ABC):
         pass
         # raise NotImplementedError("No se debe usar DAO, es una interfaz")
 
-
 class DAO_CSV_Director(DAO):
-    def __init__(self, path):  # path es la ruta que abre el csv
-        self.path = path
+  def __init__(self, path):  # path es la ruta que abre el csv
+    self.path = path
 
-    def todos(self):
+  def todos(self):
         # with open("data/directores.csv","r", newline= "") as fichero:
-        with open(self.path, "r", newline="") as fichero:
-            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
-            lista = []
-            for registro in lector_csv:
-                lista.append(Director(registro["nombre"], registro["id"]))
-        return lista
+    with open(self.path, "r", newline="") as fichero:
+      lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+            
+      lista = []
+      for registro in lector_csv:
+        lista.append(Director(registro["nombre"], int(registro["id"])))
+
+    return lista
--- tests/test_modelos.py
+++ tests/test_modelos.py
@@ -1,16 +1,60 @@
-from app.modelos import Director, DAO_CSV_Director
+from app.modelos import Director, DAO_CSV_Director, Pelicula
 
 
 def test_create_director():
+    # Cuando se crea un nuevo Director, si no se especifica un id, se asigna -1 por defecto
+    # se conoce tambien como Placeholder
     director = Director("Robert Redford")
 
     assert director.nombre == "Robert Redford"
-    assert director.id == -1
+    assert (
+        director.id == -1
+    )  # se usa para representar un id que aún no ha sido asignado
 
 
 def test_dao_directores_traer_todos():
-    dao = DAO_CSV_Director("tests/data/directores.csv")  #redefinimos
+    dao = DAO_CSV_Director("tests/data/directores.csv")
     directores = dao.todos()
 
     assert len(directores) == 8
     assert directores[7] == Director("Charlie Chaplin", 8)
+
+
+def test_create_pelicula():
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", 9)
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula._id_director == 9
+    assert (
+        pelicula.id == -1
+    )  # se usa para representar un id que aún no ha sido asignado
+    assert (
+        pelicula.director is None
+    )  # es un operador de comparación, aqui preguntas de identidad
+
+
+def test_create_pelicula_and_informar_director_completo():
+    director = Director("Peter Jackson", 9)
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", director)
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula._id_director == 9
+    assert pelicula.id == -1
+    assert pelicula.director == director
+
+
+def test_asigna_director_a_pelicula():
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", -1)
+
+    director = Director("Peter Jackson", 9)
+
+    pelicula.director = director
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+
+    assert pelicula.id == -1
+    assert pelicula.director == director
+    assert pelicula._id_director == 9
