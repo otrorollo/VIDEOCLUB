bc141290699b80cea7e0850eadf50b18ef32de1b
Ruben Ponce <rubenponcebusiness@gmail.com>
2024-08-17 19:37:07 +0200

    Resolver función que devuelve siempre lista de diccionarios

--- app/modelos.py
+++ app/modelos.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 import csv
 
-class Model(ABC): #SE CREA LA INTERFAZ - HEREDA DE CLASE ABSTRACTA ABC
+
+class Model(ABC):  # SE CREA LA INTERFAZ - HEREDA DE CLASE ABSTRACTA ABC
     @classmethod
-    @abstractmethod #EL ORDEN AL PARECER ES IMPORTANTE, DEBE IR DEBAJO DE CLASSMETHOD PARA QUE NO DE ERROR DE PYTEST
+    @abstractmethod  # EL ORDEN AL PARECER ES IMPORTANTE, DEBE IR DEBAJO DE CLASSMETHOD PARA QUE NO DE ERROR DE PYTEST
     def create_from_dict(cls, diccionario):
         pass
+
 
 class Director:
     @classmethod
@@ -16,7 +18,9 @@ class Director:
         self.nombre = nombre
         self.id = id
 
-    def __repr__(self,) -> str:  # Devuelve una cadena que representa al objeto de manera oficial. Está destinado a ser un string que permita recrear el objeto.
+    def __repr__(
+        self,
+    ) -> str:  # Devuelve una cadena que representa al objeto de manera oficial. Está destinado a ser un string que permita recrear el objeto.
         return f"Director ({self.id}): {self.nombre}"
 
     def __eq__(self, other):
@@ -26,14 +30,18 @@ class Director:
 
     def __hash__(self):
         return hash((self.id, self.nombre))
+
+
 class Pelicula:
     @classmethod
     def create_from_dict(cls, diccionario):
-        return cls(diccionario["titulo"], 
-                diccionario["sinopsis"], 
-                int(diccionario["director_id"]), 
-                int(diccionario["id"]))
-    
+        return cls(
+            diccionario["titulo"],
+            diccionario["sinopsis"],
+            int(diccionario["director_id"]),
+            int(diccionario["id"]),
+        )
+
     def __init__(self, titulo: str, sinopsis: str, director: object, id=-1):
         self.titulo = titulo
         self.sinopsis = sinopsis
@@ -70,6 +78,8 @@ class Pelicula:
 
     def __repr__(self):
         return f"Pelicula ({self.id}): {self.titulo}, {self.director}"
+
+
 class DAO(ABC):
     """
     #def __init__(self, *args)
@@ -91,15 +101,20 @@ class DAO(ABC):
       pass
       #raise NotImplementedError("No se debe usar DAO, es una interfaz")
     """
+
     @abstractmethod
     def todos(self):
         pass
         # raise NotImplementedError("No se debe usar DAO, es una interfaz")
+
+
 class DAO_CSV(DAO):  # ESTO SE HA QUITADO DE LAS SIGUIENTES CLASS 2 DE ABAJO
-    model = None 
+    model = None
+
     def __init__(self, path: str, encoding="utf-8"):
         self.path = path
         self.encoding = encoding
+
     def todos(self):
         # with open("data/directores.csv","r", newline= "") as fichero:
         with open(self.path, "r", encoding=self.encoding, newline="") as fichero:
@@ -107,23 +122,29 @@ class DAO_CSV(DAO):  # ESTO SE HA QUITADO DE LAS SIGUIENTES CLASS 2 DE ABAJO
             lista = []
             for registro in lector_csv:
                 lista.append(self.model.create_from_dict(registro))
-        return lista    
+        return lista
+
+
 class DAO_CSV_Director(DAO_CSV):
     """def todos(self):
-        # with open("data/directores.csv","r", newline= "") as fichero:
-        with open(self.path, "r", encoding="utf-8", newline="") as fichero:
-            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
-            lista = []
-            for registro in lector_csv:
-                lista.append(Director.create_from_dict(registro))
-        return lista"""
+    # with open("data/directores.csv","r", newline= "") as fichero:
+    with open(self.path, "r", encoding="utf-8", newline="") as fichero:
+        lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+        lista = []
+        for registro in lector_csv:
+            lista.append(Director.create_from_dict(registro))
+    return lista"""
+
     model = Director
+
+
 class DAO_CSV_Pelicula(DAO_CSV):
     """def todos(self):
-        with open(self.path, "r", encoding="utf-8", newline="") as fichero:
-            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
-            lista = []
-            for registro in lector_csv:
-                lista.append(Pelicula.create_from_dict(registro))
-        return lista"""
+    with open(self.path, "r", encoding="utf-8", newline="") as fichero:
+        lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+        lista = []
+        for registro in lector_csv:
+            lista.append(Pelicula.create_from_dict(registro))
+    return lista"""
+
     model = Pelicula
--- etudes/leersqlite.py
+++ etudes/leersqlite.py
@@ -1,24 +1,47 @@
 import sqlite3
 
+
+def rows_to_dictlist(filas, nombres):
+    registros = []
+    for fila in filas:
+        registro = {}
+        pos = 0
+        for nombre in nombres:
+            registro[nombre] = fila[pos]
+            pos += 1
+
+        """
+        for pos, nombre in enumerate(nombres):
+            registro[nombre] = fila[pos]
+        """
+        registros.append(registro)
+    return registros
+
+
 # Abrir conexion
 con = sqlite3.connect("data/peliculas.sqlite")
 
-# Crear cursor 
+# Crear cursor
 cur = con.cursor()
 
 # Uso el cursos con sql en forma de cadena
-cur.execute("select id, nombre, url_foto, url_web from directores")
+cur.execute("select id, nombre, url_foto from directores where url_foto is not NULL")
 
 columns_description = cur.description
+nombres_columna = []
+for columna in columns_description:
+    nombres_columna.append(columna[0])
+
+nombres_columna = list(map(lambda item: item[0], columns_description))
 
 # Proceso la respuesta si la hubiera (un select)
-result = cur.fetchall()
-
+rows = cur.fetchall()
 
 # hacer una funcion que me transforme la lista de tuplas result, en una lista de diccionarios como la que devuelve el dict reader
 
+resultado = rows_to_dictlist(rows, nombres_columna)
 
-print(result)
+print(resultado)
 
 # Cerrar la conexion siempre
 con.close()
